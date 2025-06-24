e153a357e353aad1d5bbdd536cd5af7e8a080780
Chris Morton <digiext@digitalunderworlds.com>
2025-06-24 13:33:35 -0500

    Continue pages

--- nav.php
+++ nav.php
@@ -1,11 +1,39 @@
-<nav class="navbar bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand">
-            <img src="images/atp.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+        <a class="navbar-brand" href="#">
+            <img src="images/atp.png" alt="Logo" width="30" height="24">
             All Things Puzzles</a>
-        <form class="d-flex" role="search">
-            <button class="btn btn-outline-success" type="submit">Login</button>
-            <button class="btn btn-outline-success" type="submit">Sign Up</button>
-        </form>
-    </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            <div class="text-end">
+                <button class="btn btn-outline-light" type="submit">Login</button>
+                <button class="btn btn-warning" type="submit">Sign Up</button>
+            </div>
+        </div>
 </nav>
