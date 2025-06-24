dc5f0c9e6ac91150f705bfaf1cef405a46650b52
Chris Morton <digiext@digitalunderworlds.com>
2025-06-24 14:50:01 -0500

    Continue initial pages

--- index.php
+++ index.php
@@ -7,8 +7,18 @@ include 'nav.php';
 
 ?>
 <div class="container-fluid">
+    <br>
     <h1>Welcome to All Things Puzzles!</h1>
     <br>
-    <h4>All Things Puzzles is a inventory management system of your personal puzzle collection. The system will also allow multiple users to track their
-        inventory against a master list of puzzles.</h4>
+    <h4>All Things Puzzles is a inventory management system of your personal puzzle collection. </h4>
+    <br>
+    <h3>Features
+        <ul>
+            <li>Master Puzzle List</li>
+            <li>Multi-user capable</li>
+            <li>Wishlist</li>
+        </ul>
+    </h3>
+    <br>
+    <h3>To get started, sign up for an account using the Sign Up button in the upper right corner.</h3>
 </div>
