c5ca2060229f77bceb774119dd9c8d55394a2d90
Jonathan Morton <jmorton@digitalunderworlds.com>
2025-06-26 16:58:06 -0500

    update brands admin page
    Took 23 minutes

---
+++ admin/brands.php
@@ -0,0 +1,25 @@
+<?php
+global $db;
+include '../util/db.php';
+include '../util/function.php';
+
+use \puzzlethings\src\gateway\BrandGateway;
+use puzzlethings\src\object\Brand;
+
+//If Not Logged In Reroute to index.php
+if (!isAdmin()) {
+    header("Location: home.php");
+}
+
+$title = 'Brands';
+include 'header.php';
+include 'nav.php';
+
+$gateway = new BrandGateway($db);
+$gateway->findAll();
+
+foreach ($gateway as $brand) {
+    if (!($brand instanceof Brand)) continue;
+
+    echo "Brand ID: " . $brand->getId() . " | Brand Name: " . $brand->getName();
+}
