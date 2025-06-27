fb62da249e5c03db34a17611a7ad4a633494e097
Chris Morton <cmorton@dumail.net>
2025-06-26 20:05:42 -0500

    change home button and table style

--- admin/brands.php
+++ admin/brands.php
@@ -22,13 +22,13 @@ $brands = $gateway->findAll();
 <div class="container my-2 text-center">
     <h3 class="text-center">Brand Table</h3>
     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-        <a class="btn btn-primary me-md-2" href="../home.php">Home</a>
+        <a class="btn btn-primary me-md-2" href="../admin.php">Admin Home</a>
         <a class="btn btn-primary" href="brandadd.php">Add New</a>
     </div>
 </div>
 
 <div class="container my-2">
-    <table class="table table-dark table-striped">
+    <table class="table table-success table-striped">
         <thead>
             <tr>
                 <th scope="col">#</th>
