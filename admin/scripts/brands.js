b5e059b17d0bb2457dd16b2b6bd65623ec9f18a0
Jonathan Morton <jmorton@digitalunderworlds.com>
2025-06-27 11:37:39 -0500

    fix admin brands page
    Took 2 hours 1 minute

---
+++ admin/scripts/brands.js
@@ -0,0 +1,31 @@
+$(function () {
+    $('.edit').on('click', function () {
+        let row = $(this).parent().parent();
+        let rowId = row.children('.id');
+        let rowBrand = row.children('.name');
+
+        let modalId = $("#editId");
+        let modalBrand = $("#editBrand");
+
+        modalId.val(rowId.html())
+        modalBrand.val(rowBrand.html())
+    })
+
+    $('.delete').on('click', function () {
+        let row = $(this).parent().parent();
+        let rowId = row.children('.id');
+        let rowBrand = row.children('.name');
+
+        let modalId = $("#deleteId");
+        let modalBrand = $("#deleteBrand");
+
+        modalId.val(rowId.html())
+        modalBrand.val(rowBrand.html())
+    })
+
+    $('.editSubmit').on('click', function () {
+        $(':disabled').each(function () {
+            $(this).removeAttr('disabled')
+        })
+    })
+})
