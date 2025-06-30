44fb89121fd9a9a2dd34f943327e36ab1d116118
Chris Morton <digiext@digitalunderworlds.com>
2025-06-30 13:11:00 -0500

    disposition pages

---
+++ admin/dispositionaddc.php
@@ -0,0 +1,23 @@
+<?php
+
+use puzzlethings\src\gateway\DispositionGateway;
+
+global $db;
+require_once '../util/function.php';
+require_once '../util/db.php';
+
+if (isset($_POST['submit'])) {
+    $disposition = $_POST['disposition'];
+
+    $gateway = new DispositionGateway($db);
+    $code = $gateway->create($disposition);
+
+    session_start();
+    if ($code == false) {
+        failAlert("Disposition Not Created!");
+    } else {
+        successAlert("Disposition has been created");
+    }
+
+    header("Location: disposition.php");
+}
---
+++ admin/dispositiondelete.php
@@ -0,0 +1,22 @@
+<?php
+
+use puzzlethings\src\gateway\DispositionGateway;
+
+global $db;
+require_once '../util/function.php';
+require_once '../util/db.php';
+
+$id = $_POST['id'];
+$disposition = $_POST['disposition'];
+
+$gateway = new DispositionGateway($db);
+$code = $gateway->delete($id);
+
+session_start();
+if (!$code) {
+    failAlert("Error while deleting disposition '$disposition'!");
+} else {
+    successAlert("Disposition '$disposition' (ID $id) has been deleted");
+}
+
+header("Location: disposition.php");
