05254c02627b9f65ff4ae4908de8d751e8eb445b
Chris Morton <digiext@digitalunderworlds.com>
2025-06-30 13:21:26 -0500

    ownership pages

---
+++ admin/ownership.php
@@ -0,0 +1,141 @@
+<?php
+global $db;
+include '../util/function.php';
+require '../util/db.php';
+
+use puzzlethings\src\gateway\OwnershipGateway;
+use puzzlethings\src\object\Ownership;
+
+//If Not Logged In Reroute to index.php
+if (!isLoggedIn()) {
+    header("Location: ../home.php");
+}
+
+if (!isAdmin()) {
+    header("Location: ../admin.php");
+}
+
+$title = 'Ownership';
+include '../header.php';
+include '../nav.php';
+
+$gateway = new OwnershipGateway($db);
+$ownerships = $gateway->findAll();
+?>
+
+<script src="scripts/ownership.js"></script>
+
+<!--<div class="container mb-2 mt-5 hstack">-->
+<!--    <h3 class="text-center">Brand Table</h3>-->
+<!--    <div class="d-grid gap-2 d-md-flex ms-auto">-->
+<!--        <a class="btn btn-primary me-md-2" href="../admin.php">Admin Home</a>-->
+<!--        <a class="btn btn-warning" href="brandadd.php">Add New</a>-->
+<!--    </div>-->
+<!--</div>-->
+<div class="container mb-2 mt-4 hstack justify-content-between">
+    <h3 class="text-center align-text-bottom">Ownership Table</h3>
+    <div class="d-grid gap-2 d-md-flex">
+        <a class="btn btn-primary" href="../admin.php">Admin Home</a>
+        <a class="btn btn-warning" href="ownershipadd.php">Add New</a>
+        <div class="row buttons-toolbar d-grid gap-2 d-md-flex"></div>
+    </div>
+</div>
+
+<div class="container my-2">
+    <table
+        id="table"
+        data-classes="table table-dark table-bordered table-striped table-hover"
+        data-toggle="table"
+        data-pagination="true"
+        data-search="true"
+        data-buttons-toolbar=".buttons-toolbar"
+        data-page-list="10,25,50,100,all"
+        data-search-on-enter-key="false"
+        data-id-field="id">
+        <thead>
+            <tr>
+                <th scope="col" class="text-center align-middle" data-sortable="true" data-field="id">ID</th>
+                <th scope="col" class="col-11 align-middle" data-sortable="true">Ownership</th>
+                <th scope="col" class="text-center">Edit</th>
+                <th scope="col" class="text-center">Delete</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            <?php foreach ($ownerships as $ownership) {
+                if (!($ownership instanceof Ownership)) continue;
+                echo
+                "<tr class='brand-row'>
+                    <th scope='row' class='text-center align-middle id''>" . $ownership->getId() . "</th>
+                    <td class='align-middle name'>" . $ownership->getDescription() . "</td>
+                    <td class='text-center'><button class='btn btn-secondary edit' type='submit' data-bs-toggle='modal' data-bs-target='#edit'><i class='bi bi-pencil'></td>
+                    <td class='text-center'><button class='btn btn-secondary delete' type='submit' data-bs-toggle='modal' data-bs-target='#delete'><i class='bi bi-trash'></td>
+                </tr>";
+            } ?>
+        </tbody>
+    </table>
+</div>
+
+<!-- Delete Confirmation Modal -->
+<div class="modal fade" id="delete" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="deleteLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="deleteLabel">Delete Confirmation</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form class="column" action="ownershipdelete.php" method="post" name="login">
+                <div class="modal-body">
+                    <div class="alert alert-danger" role="alert">Are you <strong>sure</strong> you want to delete this ownership?</div>
+                    <div class="col-auto">
+                        <label for="deleteId" class="col-form-label">ID</label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="text" class="form-control" id="deleteId" name="id" value="<?php echo $ownership->getId(); ?>" readonly>
+                    </div>
+                    <div class="col-auto">
+                        <label for="deleteOwnership" class="col-form-label">Ownership</label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="text" class="form-control" id="deleteOwnership" name="ownership" value="<?php echo $ownership->getDescription(); ?>" readonly>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">No</button>
+                    <button type="submit" class="btn btn-success" name="submit">Yes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Edit Modal -->
+<div class="modal fade" id="edit" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editLabel">Edit</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form class="column" action="ownershipedit.php" method="post" name="ownershipedit">
+                <div class="modal-body">
+                    <div class="col-auto">
+                        <label for="editId" class="col-form-label">ID</label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="text" class="form-control" id="editId" name="id" value="<?php echo $ownership->getId(); ?>" readonly>
+                    </div>
+                    <div class="col-auto">
+                        <label for="editOwnership" class="col-form-label">Ownership</label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="text" class="form-control" id="editOwnership" name="ownership" value="<?php echo $ownership->getDescription(); ?>">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-success" id="editSubmit" name="submit">Update</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
