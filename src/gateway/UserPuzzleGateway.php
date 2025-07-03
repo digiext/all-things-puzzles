ef840647e82ed552cbac5692fa16d4aa808c3dcc
Chris Morton <digiext@digitalunderworlds.com>
2025-07-03 08:50:17 -0500

    userpuzzle gateway

---
+++ src/gateway/UserPuzzleGateway.php
@@ -0,0 +1,304 @@
+<?php
+
+namespace puzzlethings\src\gateway;
+
+use PDO;
+use PDOException;
+use puzzlethings\src\object\{
+    Ownership,
+    Puzzle,
+    Status,
+    User,
+    UserPuzzle
+};
+
+class UserPuzzleGateway
+{
+    private PDO $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function create(User|int $user, Puzzle|int $puzzle, Status|int $status, int $missingpieces, string $start, string $end, int $totaldays, float $difficultyrating, float $qualityrating, Ownership|int $ownership): UserPuzzle|false
+    {
+        $sql = "INSERT INTO userinv (userid, puzzleid, statusid, missingpieces, startdate, enddate, totaldays, difficultyrating, qualityrating, ownershipid) VALUES (:userid, :puzzleid, :statusid, :missingpieces, :startdate, :enddate, :totaldays, :difficultyrating, :qualityrating, :ownershipid)";
+
+        $startdate = date("Y-m-d H:i:s", strtotime($start));
+        $enddate = date("Y-m-d H:i:s", strtotime($end));
+        $userId = $user instanceof User ? $user->getId() : $user;
+        $puzzleId = $puzzle instanceof Puzzle ? $puzzle->getId() : $puzzle;
+        $statusId = $status instanceof Status ? $status->getId() : $status;
+        $ownershipId = $ownership instanceof Ownership ? $ownership->getId() : $ownership;
+
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':userid', $userId);
+            $stmt->bindParam(':puzzleid', $puzzleId);
+            $stmt->bindParam(':statusid', $statusId);
+            $stmt->bindParam(':missingpieces', $missingpieces);
+            $stmt->bindParam(':startdate', $startdate);
+            $stmt->bindParam(':enddate', $enddate);
+            $stmt->bindParam(':totaldays', $totaldays);
+            $stmt->bindParam(':difficultyrating', $difficultyrating);
+            $stmt->bindParam(':qualityrating', $qualityrating);
+            $stmt->bindParam(':ownershipid', $ownershipId);
+            $stmt->execute();
+
+            $id = $this->db->lastInsertId();
+            return UserPuzzle::of([
+                "userinvid" => $id,
+                "userid" => $userId,
+                "puzzleid" => $puzzleId,
+                "statusid" => $statusId,
+                "missingpieces" => $missingpieces,
+                "startdate" => $startdate,
+                "enddate" => $enddate,
+                "totaldays" => $totaldays,
+                "difficultyrating" => $difficultyrating,
+                "qualityrating" => $qualityrating,
+                "ownershipid" => $ownershipId
+            ], $this->db);
+        } catch (PDOException $e) {
+            error_log("Database error while adding puzzle: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function count($options = [
+        FILTERS => []
+    ]): int
+    {
+        $filters = $this->determineFilters($options[FILTERS] ?? []);
+        $sql = "SELECT COUNT(*) FROM userinv $filters";
+
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchColumn();
+        } catch (PDOException $e) {
+            error_log("Database error while counting puzzles: " . $e->getMessage());
+            return -1;
+        }
+    }
+
+    public function findAll(mixed $options = [
+        PAGE => 0,
+        MAX_PER_PAGE => 10,
+        SORT => null,
+        SORT_DIRECTION => SQL_SORT_ASC,
+        FILTERS => []
+    ]): array
+    {
+        require_once __DIR__ . '/../../util/constants.php';
+
+        $sort = $options[SORT] ?? PUZ_ID;
+        $sortDirection = $options[SORT_DIRECTION] ?? SQL_SORT_ASC;
+        $page = $options[PAGE] ?? 0;
+        $maxPerPage = $options[MAX_PER_PAGE] ?? 10;
+        $filters = $options[FILTERS] ?? [];
+
+        $offset = $page * $maxPerPage;
+        $filters = $this->determineFilters($filters);
+
+        $sql = "SELECT * FROM userinv $filters ORDER BY $sort $sortDirection LIMIT $offset, $maxPerPage";
+
+        try {
+            $stmt = $this->db->query($sql);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $puzzles = array();
+
+            foreach ($result as $res) {
+                $puzzles[] = UserPuzzle::of($res, $this->db);
+            }
+
+            return $puzzles;
+        } catch (PDOException $e) {
+            error_log('Error while fetching puzzles: ' . $e->getMessage());
+            die();
+        }
+    }
+
+    private function determineFilters(mixed $filters = []): string
+    {
+        $res = "";
+
+        // foreach ($filters as $filter => $val) {
+        //     switch ($filter) {
+        //         case PUZ_FILTER_NAME: {
+        //                 $res .= "AND puzname LIKE %" . $val . "% ";
+        //                 break;
+        //             }
+        //         case PUZ_FILTER_PIECES: {
+        //                 if (is_array($val)) {
+        //                     $res .= "AND pieces BETWEEN $val[0] AND $val[1]";
+        //                 } else {
+        //                     $res .= "AND pieces = $val";
+        //                 }
+        //                 break;
+        //             }
+        //         case PUZ_FILTER_BRAND: {
+        //                 if ($val instanceof Brand) {
+        //                     $id = $val->getId();
+        //                     $res .= "AND brandid = $id";
+        //                 } else {
+        //                     $res .= "AND brandid = $val";
+        //                 }
+        //                 break;
+        //             }
+        //         case PUZ_FILTER_COST: {
+        //                 if (is_array($val)) {
+        //                     $res .= "AND cost BETWEEN $val[0] AND $val[1]";
+        //                 } else {
+        //                     $res .= "AND cost = $val";
+        //                 }
+        //                 break;
+        //             }
+        //         case PUZ_FILTER_SOURCE: {
+        //                 if ($val instanceof Source) {
+        //                     $id = $val->getId();
+        //                     $res .= "AND sourceid = $id";
+        //                 } else {
+        //                     $res .= "AND sourceid = $val";
+        //                 }
+        //                 break;
+        //             }
+        //         case PUZ_FILTER_LOCATION: {
+        //                 if ($val instanceof Location) {
+        //                     $id = $val->getId();
+        //                     $res .= "AND locationid = $id";
+        //                 } else {
+        //                     $res .= "AND locationid = $val";
+        //                 }
+        //                 break;
+        //             }
+        //         case PUZ_FILTER_DISPOSITION: {
+        //                 if ($val instanceof Disposition) {
+        //                     $id = $val->getId();
+        //                     $res .= "AND dispositionid = $id";
+        //                 } else {
+        //                     $res .= "AND dispositionid = $val";
+        //                 }
+        //                 break;
+        //             }
+        //     }
+        // }
+
+        $pos = strpos($res, "AND");
+        if ($pos !== false) {
+            $res = substr_replace($res, "WHERE", $pos, 3);
+        }
+
+        return $res;
+    }
+
+    public function findById(int $id, mixed $options = []): ?UserPuzzle
+    {
+        $sql = "SELECT * FROM userinv WHERE userinvid = :id";
+
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+            if ($stmt->rowCount() == 0) return null;
+
+            return UserPuzzle::of($result, $this->db);
+        } catch (PDOException $e) {
+            return null;
+        }
+    }
+
+    // public function findByName(string $name, mixed $options = []): ?Puzzle
+    // {
+    //     $sql = "SELECT * FROM puzzleinv WHERE name LIKE :name";
+
+    //     $before = null;
+    //     $after = null;
+    //     if ($options['before'] != null && $options['after'] != null) {
+    //         $before = $options['before']->format('Y-m-d H:i:s');
+    //         $after = $options['after']->format('Y-m-d H:i:s');
+    //         $sql .= " AND dateacquired BETWEEN :after AND :before";
+    //     } else if ($options['before'] != null) {
+    //         $before = $options['before']->format('Y-m-d H:i:s');
+    //         $sql .= " AND dateacquired < :before";
+    //     } else if ($options['after'] != null) {
+    //         $after = $options['after']->format('Y-m-d H:i:s');
+    //         $sql .= " AND dateacquired > :after";
+    //     }
+
+    //     try {
+    //         $likeName = "%" . $name . "%";
+
+    //         $stmt = $this->db->prepare($sql);
+    //         $stmt->bindParam(':name', $likeName);
+    //         if ($before != null) $stmt->bindParam(':before', $before);
+    //         if ($after != null) $stmt->bindParam(':after', $after);
+
+    //         $stmt->execute();
+    //         $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    //         if ($stmt->rowCount() == 0) return null;
+
+    //         return Puzzle::of($result, $this->db);
+    //     } catch (PDOException $e) {
+    //         print($e->getMessage());
+    //         return null;
+    //     }
+    // }
+
+    public function update(UserPuzzle|int $puzzle, array $values): bool
+    {
+        if (empty($puzzle)) return false;
+        $id = $puzzle instanceof UserPuzzle ? $puzzle->getId() : $puzzle;
+
+        $sets = "";
+        foreach ($values as $key => $value) {
+            $sets .= "$key = :$key, ";
+        }
+
+        if ($sets == '') return false;
+
+        $sql = "UPDATE userinv SET $sets WHERE puzzleid = :puzzleid";
+
+        $pos = strrpos($sql, ", ");
+        if ($pos !== false) {
+            $sql = substr_replace($sql, "", $pos, 2);
+        }
+
+        try {
+            $stmt = $this->db->prepare($sql);
+            // $stmt->bindParam(':puzzleid', $id, PDO::PARAM_INT);
+
+            $exec = [
+                ":puzzleid" => $id,
+            ];
+            foreach ($values as $key => $value) {
+                $exec[":$key"] = $value;
+                // $stmt->bindParam(":$key", $value, is_numeric($value) ? PDO::PARAM_INT : PDO::PARAM_STR);
+            }
+
+            return $stmt->execute($exec);
+        } catch (PDOException $e) {
+            error_log("Database error while updating puzzle: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function delete(UserPuzzle|int $id): bool
+    {
+        $sql = "DELETE FROM userinv WHERE userinvid = :userinvid";
+
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':userinvid', $id);
+            return $stmt->execute();
+        } catch (PDOException $e) {
+            error_log("Database error while deleting puzzle: " . $e->getMessage());
+            return false;
+        }
+    }
+}
---
+++ src/object/UserPuzzle.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace puzzlethings\src\object;
+
+use puzzlethings\src\gateway\StatusGateway;
+use puzzlethings\src\gateway\OwnershipGateway;
+use puzzlethings\src\gateway\UserGateway;
+use puzzlethings\src\gateway\PuzzleGateway;
+
+class UserPuzzle implements \JsonSerializable
+{
+    private int $id;
+    private ?string $start, $end;
+    private ?int $missingpieces, $totaldays;
+    private ?float $difficultyrating, $qualityrating;
+    private ?User $user;
+    private ?Puzzle $puzzle;
+    private ?Status $status;
+    private ?Ownership $ownership;
+
+
+
+    public function __construct(int $id, ?User $user, ?Puzzle $puzzle, ?Status $status, ?int $missingpieces, ?string $start, ?string $end, ?int $totaldays, ?float $difficultyrating, ?float $qualityrating, ?Ownership $ownership)
+    {
+        $this->id = $id;
+        $this->user = $user;
+        $this->puzzle = $puzzle;
+        $this->status = $status;
+        $this->missingpieces = $missingpieces;
+        $this->start = $start;
+        $this->end = $end;
+        $this->totaldays = $totaldays;
+        $this->difficultyrating = $difficultyrating;
+        $this->qualityrating = $qualityrating;
+        $this->ownership = $ownership;
+    }
+
+    public static function of($res, $db): UserPuzzle
+    {
+        $user = (new UserGateway($db))->findById($res['userid']);
+        $puzzle = (new PuzzleGateway($db))->findById($res['puzzleid']);
+        $status = (new StatusGateway($db))->findById($res['statusid']);
+        $ownership = (new OwnershipGateway($db))->findById($res['ownershipid']);
+
+        return new UserPuzzle($res['userinvid'], $user, $puzzle, $status, $res['missingpieces'], $res['start'], $res['end'], $res['totaldays'], $res['difficultyrating'], $res['qualityrating'], $ownership);
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user;
+    }
+
+    public function getPuzzle(): ?Puzzle
+    {
+        return $this->puzzle;
+    }
+
+    public function getStatus(): ?Status
+    {
+        return $this->status;
+    }
+
+    public function getMissingPieces(): ?int
+    {
+        return $this->missingpieces;
+    }
+
+    public function getStart(): ?string
+    {
+        return $this->start;
+    }
+
+    public function getEnd(): ?string
+    {
+        return $this->end;
+    }
+
+    public function getTotal(): ?int
+    {
+        return $this->totaldays;
+    }
+
+    public function getDifficulty(): ?float
+    {
+        return $this->difficultyrating;
+    }
+
+    public function getQuality(): ?float
+    {
+        return $this->qualityrating;
+    }
+
+    public function getOwnership(): ?Ownership
+    {
+        return $this->ownership;
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return [
+            'id' => $this->id,
+            'user' => $this->user,
+            'puzzle' => $this->puzzle,
+            'status' => $this->status,
+            'missingpieces' => $this->missingpieces,
+            'start' => $this->start,
+            'end' => $this->end,
+            'totaldays' => $this->totaldays,
+            'difficultyrating' => $this->difficultyrating,
+            'qualityrating' => $this->qualityrating,
+            'ownership' => $this->ownership,
+        ];
+    }
+}
