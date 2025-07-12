8358757c3f34269c7c72e669ae4224321370726e
Chris Morton <cmorton@dumail.net>
2025-07-12 06:44:58 -0500

    change version number

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="fixed-bottom d-none d-sm-block bg-body-tertiary text-center text-lg-start">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-        <p>Version: 0.5<br>
+        <p>Version: 7122025<br>
             Developed by <strong>Chris & Jonathan Morton</strong></p>
     </div>
 </footer>
