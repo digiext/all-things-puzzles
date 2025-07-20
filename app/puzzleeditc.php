81c9760703bd8ea39bf41c83bbfa2c5bba36ece5
Chris Morton <cmorton@dumail.net>
2025-07-20 13:51:37 -0500

    debug 11

--- app/puzzleeditc.php
+++ app/puzzleeditc.php
@@ -98,6 +98,7 @@ if (isset($_POST['submit'])) {
 
             var_dump($filepath);
             $success = move_uploaded_file($tmp, $filepath);
+            echo $success;
             if ($success) {
                 $picture = $uploadedFile;
             }
